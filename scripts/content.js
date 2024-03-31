7d446d6ce0d35c27f63651f7f1d7966fcc022948
Kaushik <akaushikarkar@gmail.com>
2024-03-31 22:08:25 +0530

    basic setup

---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+// const article = document.querySelector(".playback-bar");
+// console.log(article)
+
+while (true) {
+    const article = document.querySelector(".playback-bar");
+    console.log(article)
+    setTimeout(() => {
+      
+    }, 5000);
+  }
